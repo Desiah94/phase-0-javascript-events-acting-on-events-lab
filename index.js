a94a8fd1647da542ed8f124c88b7e1703316d9cf
Desiah Barnett <desiahbarnett@gmail.com>
2023-12-30 16:36:29 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Your code here
+const dodger = document.getElementById('dodger');
+
+function moveDodgerLeft() {
+  const currentPosition = parseInt(dodger.style.left);
+  const newPosition = currentPosition - 4;
+  if (newPosition >= 0) {
+    dodger.style.left = '176px';
+  }
+}
+function moveDodgerRight() {
+    let leftNumbers = dodger.style.left.replace('px', '')
+    let left = parseInt(leftNumbers, 10)
+
+    if (left < 360)
+      dodger.style.left = `${left + 1}px`
+}
